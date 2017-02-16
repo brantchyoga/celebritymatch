51d759c18c510ee8dd185048c74cb060e0130326
brant porter <brantrporter@gmail.com>
2017-02-16 14:14:36 -0800

    Show 3 options and show celebrity match

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  $("form#choice").submit(function(event){
+    event.preventDefault();
+    var animal = $("#animals").val();
+    var gender = $("#gender").val();
+    var music = $("input:radio[name=music]:checked").val();
+    console.log(gender);
+    console.log(animal);
+    if (gender === "male" && (music === "alt" || music === "class") && (animal === "turtle" || animal === "tiger")) {
+      console.log(gender);
+      $("#rock").show();
+    } else if (gender === "male" && music === "rap" && (animal ==="all of them"|| animal ==="snake")) {
+      $("#jb").show();
+    } else if (gender === "female" && (music === "alt"|| music === "class") && (animal === "turtle"|| animal === "tiger")) {
+      $("#ja").show();
+    } else if (gender === "female" && music === "rap" && (animal ==="all of them"|| animal ==="snake")) {
+      $("#kk").show();
+    } else if (gender === "female") {
+      $("#jb").show();
+      console.log("last male option");
+    } else {
+      $("#kk").show();
+      console.log("Last female option");
+    }
+    // if ( gender === "female" && music === "alt" || music === "rap" && animal === "turtle") {
+    //   $("#kk").show();
+    // } else if (gender === "female" && music === "class" && animal === "snake") {
+    //   $("#rock").show();
+    // } else {
+    //   $("#jb").show();
+    // }
+  });
+});
